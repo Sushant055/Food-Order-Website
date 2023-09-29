8353ad804a6fc5a2f3e4d066a6c22b9ec85380e9
Kavila Sushant <110624592+Sushant055@users.noreply.github.com>
2023-09-29 10:22:32 +0530

    Add files via upload
    Added partials

---
+++ partials-front/footer.php
@@ -0,0 +1,10 @@
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="https://github.com/Sushant055" target="_blank">Sushant055</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
