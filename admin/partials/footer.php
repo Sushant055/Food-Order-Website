4e9154db4c975dc2b64dbe78f25a8cb2e81069b2
Kavila Sushant <110624592+Sushant055@users.noreply.github.com>
2023-09-29 10:19:55 +0530

    Add files via upload
    added admin folder

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2023 All rights reserved, Food Order Website. Developed By - <a >Kavila_Sushant</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
